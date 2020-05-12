29ef43c419b1e65661488a5fc82293535c9f0bd0
harold1024 <744924498@qq.com>
2020-05-12 16:24:08 +0800

    响应式布局web端和移动端

---
+++ src/pages/pc/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    111
+    <div id="bg" />
+  </div>
+</template>
+
+<script>
+// import "@/tyles/_bg.scss"
+export default {
+  name: "Home",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped></style>
